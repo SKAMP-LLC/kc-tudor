13d4e476b9730fc3738455d806961ee004ffae52
Devin Leonhart <devinleonhart@fastmail.com>
2019-04-20 17:03:20 -0700

    Added descriptions to Sandy Says

--- index.php
+++ index.php
@@ -21,57 +21,57 @@ get_header(); ?>
     <div class="siema">
       <div class="comic_slot slot_0"
         data-title="iMew"
-        data-rating="All Ages"
-        data-description="This is the desc for iMew"
+        data-rating="Rated PG-13"
+        data-description="Ever think your phone's controlling your life? Sammy sure feels that way! After purchasing a mysterious knock-off phone, she's discovered the apps control a little more than you or I would expect! Now she has to get her life back before the battery runs out!"
       >
         <a href="">Comic Slot</a>
       </div>
       <div class="comic_slot slot_1"
         data-title="Paprika"
-        data-rating="Mature"
-        data-description="This is the desc for Paprika"
+        data-rating="Rated PG-13"
+        data-description="Paprika is a celebraton of anime's golden age through the eyes of some very special fans. Tina and her friends have some very strong imaginations, ones so strong they transport the girls to a whole new world when they let it run away with them."
       >
         <a href="">Comic Slot</a>
       </div>
       <div class="comic_slot slot_2"
         data-title="Caribbean Blue"
-        data-rating="Hoo now!"
-        data-description="This is the desc for CB"
+        data-rating="Rated PG-13"
+        data-description="Tina and Yuki travel to the island of Caribbean Blue in search of a cure for their friend's strange feline curse, only to discover the island has many more secrets than they bargained for. Will they save the island after accidentally unearthing one of its greatest mysteries? Read and find out!"
       >
         <a href="">Comic Slot</a>
       </div>
       <div class="comic_slot slot_3"
         data-title="PMP"
-        data-rating="Zomg..."
-        data-description="This is the desc for PMP"
+        data-rating="Rated E"
+        data-description="Nekonny's 4koma is good for laughs. Think of it like comics from the newspaper with adorable child characters and a more modern sense of humor."
       >
         <a href="">Comic Slot</a>
       </div>
       <div class="comic_slot slot_0"
         data-title="iMew"
-        data-rating="All Ages"
-        data-description="This is the desc for iMew"
+        data-rating="Rated PG-13"
+        data-description="Ever think your phone's controlling your life? Sammy sure feels that way! After purchasing a mysterious knock-off phone, she's discovered the apps control a little more than you or I would expect! Now she has to get her life back before the battery runs out!"
       >
         <a href="">Comic Slot</a>
       </div>
       <div class="comic_slot slot_1"
         data-title="Paprika"
-        data-rating="Mature"
-        data-description="This is the desc for Paprika"
+        data-rating="Rated PG-13"
+        data-description="Paprika is a celebraton of anime's golden age through the eyes of some very special fans. Tina and her friends have some very strong imaginations, ones so strong they transport the girls to a whole new world when they let it run away with them."
       >
         <a href="">Comic Slot</a>
       </div>
       <div class="comic_slot slot_2"
         data-title="Caribbean Blue"
-        data-rating="Hoo now!"
-        data-description="This is the desc for CB"
+        data-rating="Rated PG-13"
+        data-description="Tina and Yuki travel to the island of Caribbean Blue in search of a cure for their friend's strange feline curse, only to discover the island has many more secrets than they bargained for. Will they save the island after accidentally unearthing one of its greatest mysteries? Read and find out!"
       >
         <a href="">Comic Slot</a>
       </div>
       <div class="comic_slot slot_3"
         data-title="PMP"
-        data-rating="Zomg..."
-        data-description="This is the desc for PMP"
+        data-rating="Rated E"
+        data-description="Nekonny's 4koma is good for laughs. Think of it like comics from the newspaper with adorable child characters and a more modern sense of humor."
       >
         <a href="">Comic Slot</a>
       </div>
@@ -82,9 +82,9 @@ get_header(); ?>
   <section class="sandy-says">
     <div class="sandy"></div>
     <div class="speech_bubble">
-      <span class="title">Comic Title</span>
-      <span class="rating">All Ages</span>
-      <p class="description">Lorem Ipsum</p>
+      <span class="title"></span>
+      <span class="rating"></span>
+      <p class="description"></p>
       <div class="arrow"></div>
     </div>
   </section>
