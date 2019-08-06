88fe084bcf4804e9ceb24d7a654aaee2fc81e56f
Devin Leonhart <devinleonhart@fastmail.com>
2019-08-05 21:56:00 -0700

    Spelling errors and new heights.

--- template-parts/content-page-cast.php
+++ template-parts/content-page-cast.php
@@ -14,34 +14,34 @@
       <div class="left image"></div>
       <div class="right stats">
         <h1>Sandy</h1>
-        <h2>Height ~ 170cm</h2>
+        <h2>Height ~ 162cm (5'4")</h2>
         <h2>Favorite Food ~ Fried Chicken</h2>
-        <p>Imagine yourself coming into possession of a magical ticket. Behind you is a predictable life. There’s no room for dreams where you live. A bright red train pulls into the station and brings with it a promise of another world, but one entirely unknown. That train came for Sandy and, with a dream for a better life burning in her heart, she gave the ticket to the conductor and off they went. Now Sandy is the newest resident of Fable, a world between worlds, and runs the cafe there; a home for all travellers who take their tickets and follow their dreams.</p>
+        <p>Imagine yourself coming into possession of a magical ticket. Behind you is a predictable life. There’s no room for dreams where you live. A bright red train pulls into the station and brings with it a promise of another world, but one entirely unknown. That train came for Sandy and, with a dream for a better life burning in her heart, she gave the ticket to the conductor and off they went. Now Sandy is the newest resident of Fable, a world between worlds, and runs the cafe there; a home for all travelers who take their tickets and follow their dreams.</p>
       </div>
     </div>
     <div class="kona box">
       <div class="right image"></div>
       <div class="left stats">
         <h1>Kona</h1>
-        <h2>Height ~ 154cm (before ears!)</h2>
+        <h2>Height ~ 152cm (5') (before ears!)</h2>
         <h2>Favorite Food ~ Roasted Vegetables</h2>
-        <p>“All Aboard the Escapade!” Kona shouts to a bustling crowd of travellers. As one of Fable’s oldest residents, no one knows how Kona found her magic train or managed to become its conductor. She treats the steam engine like an old friend, ferrying customers back and forth between Kemono Cafe and their home worlds like it was something she was born to do. And yet, despite how wondrous her life has been, Kona feels like there’s something deeper about the train she’s on the cusp of understanding.</p>
+        <p>“All Aboard the Escapade!” Kona shouts to a bustling crowd of travelers. As one of Fable’s oldest residents, no one knows how Kona found her magic train or managed to become its conductor. She treats the steam engine like an old friend, ferrying customers back and forth between Kemono Cafe and their home worlds like it was something she was born to do. And yet, despite how wondrous her life has been, Kona feels like there’s something deeper about the train she’s on the cusp of understanding.</p>
       </div>
     </div>
     <div class="maxine box">
       <div class="left image"></div>
       <div class="right stats">
         <h1>Maxine</h1>
-        <h2>Height ~ 196cm</h2>
+        <h2>Height ~ 137cm (4'6")</h2>
         <h2>Favorite Food ~ Tea</h2>
-        <p>One question: Can you find it in Fable? Then you can find it at Maxine’s Eclectic Emporium! Mistress of mercantile and would-be-mother to every wanderer, Maxine unites the town as family. Not to say she doesn’t enjoy the creature comforts of being the richest corgi in Fable… But she’s been charitable to more than a few travellers who come to her in need. “The smile on your face is it’s own reward!” She’d say loudly and proudly. “But only the first one’s free...” She’d kindly whisper to you on the way out.</p>
+        <p>One question: Can you find it in Fable? Then you can find it at Maxine’s Eclectic Emporium! Mistress of mercantile and would-be-mother to every wanderer, Maxine unites the town as family. Not to say she doesn’t enjoy the creature comforts of being the richest corgi in Fable… But she’s been charitable to more than a few travelers who come to her in need. “The smile on your face is its own reward!” She’d say loudly and proudly. “But only the first one’s free...” She’d kindly whisper to you on the way out.</p>
       </div>
     </div>
     <div class="miko box">
       <div class="right image"></div>
       <div class="left stats">
         <h1>Miko</h1>
-        <h2>Height ~ 172cm</h2>
+        <h2>Height ~ 167cm (5'6")</h2>
         <h2>Favorite Food ~ Glazed Donuts</h2>
         <p>Does Fable really need a police officer? Technically, it does! Miko’s on the job! But the poor doggo has so little to do in the peaceful town that she’d give up her morning donut for a real perp to chase! She’s really excited about the cafe opening back up because with all the new people coming through, maybe she’ll have a real job again! But be careful when you visit Fable... If you’re caught breaking the law, Miko will give you such a warning!</p>
       </div>
@@ -50,7 +50,7 @@
       <div class="left image"></div>
       <div class="right stats">
         <h1>Jill</h1>
-        <h2>Height ~ 170cm</h2>
+        <h2>Height ~ 160cm (5'3")</h2>
         <h2>Favorite Food ~ Pecan Pie</h2>
         <p>You have to be tough to live in the Forest of Illusion and Jill’s as tough as Fable gets. She’s the town’s forester, harvesting everything that the town needs to grow. Does she need any help with such a hard job? Nah. She only trusts things she’s built with her own two hands! Watch out, Sandy. That axe of hers is sharp. Maybe you can lure her out of the woods with some pie from the cafe?</p>
       </div>
