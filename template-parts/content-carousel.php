63fe8fa10b242bd9ea6593af963a6de62bfc2b86
Devin Leonhart <devinleonhart@fastmail.com>
2019-08-14 19:00:16 -0700

    Update carousel.

--- template-parts/content-carousel.php
+++ template-parts/content-carousel.php
@@ -35,29 +35,9 @@
       data-description="Historian Ronald Rudentof has spent his entire life trying to escape his father's shadow. But while attempting to solve the last mystery his father left behind, Ron's life was changed forever. Now permanently bonded to an ancient demigod named Ramalach that has taken residence inside his forehead, Ron finds himself trapped between forces determined to either destroy Ramalach, or use him for their own sinister purposes.">
       <a href="https://theeye.kemono.cafe"></a>
     </div>
-    <div class="comic_slot slot_0" data-title="iMew" data-rating="Rated PG-13"
-      data-description="Ever think your phone's controlling your life? Sammy sure feels that way! After purchasing a mysterious knock-off phone, she's discovered the apps control a little more than you or I would expect! Now she has to get her life back before the battery runs out!">
-      <a href="https://imew.kemono.cafe"></a>
-    </div>
-    <div class="comic_slot slot_1" data-title="Paprika" data-rating="Rated PG-13"
-      data-description="Paprika is a celebration of anime's golden age through the eyes of some very special fans. Tina and her friends have some very strong imaginations, ones so strong they transport the girls to a whole new world when they let it run away with them.">
-      <a href="https://paprika.kemono.cafe"></a>
-    </div>
-    <div class="comic_slot slot_2" data-title="Caribbean Blue" data-rating="Rated PG-13"
-      data-description="Tina and Yuki travel to the island of Caribbean Blue in search of a cure for their friend's strange feline curse, only to discover the island has many more secrets than they bargained for. Will they save the island after accidentally unearthing one of its greatest mysteries? Read and find out!">
-      <a href="https://cb.kemono.cafe"></a>
-    </div>
-    <div class="comic_slot slot_3" data-title="PMP" data-rating="Rated E"
-      data-description="Nekonny's 4koma is good for laughs. Think of it like comics from the newspaper with adorable child characters and a more modern sense of humor.">
-      <a href="https://pmp.nekonny.com/"></a>
-    </div>
-    <div class="comic_slot slot_4" data-title="Rascals" data-rating="Rated PG-13"
-      data-description="Follow the crazy antics of a group of friends from different backgrounds as they grow and experience the ups and downs of days dealing with Love triangles, misunderstood situations, annoying jobs and nosy parents. Will all of them find a happy ending in this crazy thing we call Life? Let's join them and find out!">
-      <a href="https://rascals.kemono.cafe"></a>
-    </div>
-    <div class="comic_slot slot_5" data-title="The Eye" data-rating="Rated PG-13"
-      data-description="Historian Ronald Rudentof has spent his entire life trying to escape his father's shadow. But while attempting to solve the last mystery his father left behind, Ron's life was changed forever. Now permanently bonded to an ancient demigod named Ramalach that has taken residence inside his forehead, Ron finds himself trapped between forces determined to either destroy Ramalach, or use him for their own sinister purposes.">
-      <a href="https://theeye.kemono.cafe"></a>
+    <div class="comic_slot slot_6" data-title="Las Lindas" data-rating="Rated PG-13"
+      data-description="Las Lindas description.">
+      <a href="https://laslindas.kemono.cafe"></a>
     </div>
     <div class="comic_slot slot_0" data-title="iMew" data-rating="Rated PG-13"
       data-description="Ever think your phone's controlling your life? Sammy sure feels that way! After purchasing a mysterious knock-off phone, she's discovered the apps control a little more than you or I would expect! Now she has to get her life back before the battery runs out!">
@@ -82,6 +62,38 @@
     <div class="comic_slot slot_5" data-title="The Eye" data-rating="Rated PG-13"
       data-description="Historian Ronald Rudentof has spent his entire life trying to escape his father's shadow. But while attempting to solve the last mystery his father left behind, Ron's life was changed forever. Now permanently bonded to an ancient demigod named Ramalach that has taken residence inside his forehead, Ron finds himself trapped between forces determined to either destroy Ramalach, or use him for their own sinister purposes.">
       <a href="https://theeye.kemono.cafe"></a>
+    </div>
+    <div class="comic_slot slot_6" data-title="Las Lindas" data-rating="Rated PG-13"
+      data-description="Las Lindas description.">
+      <a href="https://laslindas.kemono.cafe"></a>
+    </div>
+    <div class="comic_slot slot_0" data-title="iMew" data-rating="Rated PG-13"
+      data-description="Ever think your phone's controlling your life? Sammy sure feels that way! After purchasing a mysterious knock-off phone, she's discovered the apps control a little more than you or I would expect! Now she has to get her life back before the battery runs out!">
+      <a href="https://imew.kemono.cafe"></a>
+    </div>
+    <div class="comic_slot slot_1" data-title="Paprika" data-rating="Rated PG-13"
+      data-description="Paprika is a celebration of anime's golden age through the eyes of some very special fans. Tina and her friends have some very strong imaginations, ones so strong they transport the girls to a whole new world when they let it run away with them.">
+      <a href="https://paprika.kemono.cafe"></a>
+    </div>
+    <div class="comic_slot slot_2" data-title="Caribbean Blue" data-rating="Rated PG-13"
+      data-description="Tina and Yuki travel to the island of Caribbean Blue in search of a cure for their friend's strange feline curse, only to discover the island has many more secrets than they bargained for. Will they save the island after accidentally unearthing one of its greatest mysteries? Read and find out!">
+      <a href="https://cb.kemono.cafe"></a>
+    </div>
+    <div class="comic_slot slot_3" data-title="PMP" data-rating="Rated E"
+      data-description="Nekonny's 4koma is good for laughs. Think of it like comics from the newspaper with adorable child characters and a more modern sense of humor.">
+      <a href="https://pmp.nekonny.com/"></a>
+    </div>
+    <div class="comic_slot slot_4" data-title="Rascals" data-rating="Rated PG-13"
+      data-description="Follow the crazy antics of a group of friends from different backgrounds as they grow and experience the ups and downs of days dealing with Love triangles, misunderstood situations, annoying jobs and nosy parents. Will all of them find a happy ending in this crazy thing we call Life? Let's join them and find out!">
+      <a href="https://rascals.kemono.cafe"></a>
+    </div>
+    <div class="comic_slot slot_5" data-title="The Eye" data-rating="Rated PG-13"
+      data-description="Historian Ronald Rudentof has spent his entire life trying to escape his father's shadow. But while attempting to solve the last mystery his father left behind, Ron's life was changed forever. Now permanently bonded to an ancient demigod named Ramalach that has taken residence inside his forehead, Ron finds himself trapped between forces determined to either destroy Ramalach, or use him for their own sinister purposes.">
+      <a href="https://theeye.kemono.cafe"></a>
+    </div>
+    <div class="comic_slot slot_6" data-title="Las Lindas" data-rating="Rated PG-13"
+      data-description="Las Lindas description.">
+      <a href="https://laslindas.kemono.cafe"></a>
     </div>
   </div>
   <div class="control next_slot"></div>
