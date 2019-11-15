d7dc2fc74feb35206ffdaa118e597f5f5f691b5d
Devin Leonhart <devinleonhart@fastmail.com>
2019-11-15 13:50:44 -0800

    Add favicon

--- header.php
+++ header.php
@@ -26,6 +26,7 @@
     <meta name="keywords" content="webcomic, webcomics, anime, furry, kemono, art, furry art">
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico?" />
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
