8f1b54a8030f37d6138f8372f41e1f0b1974338b
Devin Leonhart <devinleonhart@fastmail.com>
2019-12-01 15:54:28 -0800

    Finish adding emotes

--- src/js/emote.js
+++ src/js/emote.js
@@ -1,73 +1,38 @@
 import mergeImages from 'merge-images';
+import sandy from './emotes/sandy';
+import kona from './emotes/kona';
+import jill from './emotes/jill';
+import maxine from './emotes/maxine';
+import miko from './emotes/miko';
+import steve from './emotes/steve';
 
 export const assets = {
-  sandy: {
-    hair: {
-      base: require('@/emotes/sandy/sandy_hair_base.png'),
-    },
-    ears: {
-      droop: require('@/emotes/sandy/sandy_ears_droop.png'),
-      normal: require('@/emotes/sandy/sandy_ears_normal.png'),
-      perked: require('@/emotes/sandy/sandy_ears_perked.png'),
-      sad: require('@/emotes/sandy/sandy_ears_sad.png'),
-    },
-    head: {
-      base: require('@/emotes/sandy/sandy_head_base.png'),
-    },
-    eyes: {
-      angry: require('@/emotes/sandy/sandy_eyes_angry.png'),
-      astonished: require('@/emotes/sandy/sandy_eyes_astonished.png'),
-      glee: require('@/emotes/sandy/sandy_eyes_glee.png'),
-      happy: require('@/emotes/sandy/sandy_eyes_happy.png'),
-      lookaway: require('@/emotes/sandy/sandy_eyes_lookaway.png'),
-      normal: require('@/emotes/sandy/sandy_eyes_normal.png'),
-      squint: require('@/emotes/sandy/sandy_eyes_squint.png'),
-      surprised: require('@/emotes/sandy/sandy_eyes_surprised.png'),
-      uu: require('@/emotes/sandy/sandy_eyes_uu.png'),
-      x: require('@/emotes/sandy/sandy_eyes_x.png'),
-    },
-    eyebrow: {
-      angry: require('@/emotes/sandy/sandy_eyebrow_angry.png'),
-      inquisitive: require('@/emotes/sandy/sandy_eyebrow_inquisitive.png'),
-      normal: require('@/emotes/sandy/sandy_eyebrow_normal.png'),
-      sad: require('@/emotes/sandy/sandy_eyebrow_sad.png'),
-      serious: require('@/emotes/sandy/sandy_eyebrow_serious.png'),
-    },
-    mouth: {
-      formal: require('@/emotes/sandy/sandy_mouth_formal.png'),
-      pout: require('@/emotes/sandy/sandy_mouth_pout.png'),
-      sad: require('@/emotes/sandy/sandy_mouth_sad.png'),
-      serious: require('@/emotes/sandy/sandy_mouth_serious.png'),
-      smile: require('@/emotes/sandy/sandy_mouth_smile.png'),
-      smileopen: require('@/emotes/sandy/sandy_mouth_smileopen.png'),
-      yell: require('@/emotes/sandy/sandy_mouth_yell.png'),
-    },
-  }
+  sandy: sandy,
+  kona: kona,
+  jill: jill,
+  maxine: maxine,
+  miko: miko,
+  steve: steve,
 };
 
 let mergedAssets = {
-  'sandy_normal': makeEmoji('sandy_normal', 'sandy', 'base', 'normal', 'base', 'normal', 'normal', 'smile'),
-  'sandy_happy': makeEmoji('sandy_happy', 'sandy', 'base', 'normal', 'base', 'normal', 'normal', 'smileopen'),
-  'sandy_surprised': makeEmoji('sandy_surprised', 'sandy', 'base', 'perked', 'base', 'surprised', 'normal', 'yell'),
-  'sandy_oof': makeEmoji('sandy_oof', 'sandy', 'base', 'droop', 'base', 'x', 'sad', 'pout'),
-  'sandy_whoa': makeEmoji('sandy_whoa', 'sandy', 'base', 'perked', 'base', 'uu', 'serious', 'smileopen'),
+  'sandy_normal': makeEmoji('sandy_normal', 'sandy', 'normal', 'normal', 'normal', 'smile'),
+  'kona_happy': makeEmoji('kona_happy', 'kona', 'perked', 'normal', 'glee', 'widesmile'),
+  'maxine_greedy': makeEmoji('maxine_greedy', 'maxine', 'flop', 'angry', 'gleam', 'oh'),
+  'miko_whoa': makeEmoji('miko_whoa', 'miko', 'perked', 'surprised', 'happy', 'smileopen'),
+  'jill_excited': makeEmoji('jill_excited', 'jill', 'droop', 'puzzled', 'lookaway', 'yell'),
+  'steve_pringles': makeEmoji('steve_pringles', 'steve', 'normal', 'pringles', 'normal', 'grin'),
 };
 
-function makeEmoji(key, character, hair, ears, head, eyes, eyebrows, mouth) {
+function makeEmoji(key, character, head, eyebrow, eyes, mouth) {
   mergeImages([
-    assets[character].hair[hair],
-    assets[character].ears[ears],
     assets[character].head[head],
+    assets[character].eyebrow[eyebrow],
     assets[character].eyes[eyes],
-    assets[character].eyebrow[eyebrows],
     assets[character].mouth[mouth],
   ]).then((merged) => {
     mergedAssets[key] = merged;
   });
-}
-
-window.getEmoji = (id, emote) => {
-  document.getElementById(id)
 }
 
 document.addEventListener("DOMContentLoaded", () => {
---
+++ src/js/emotes/steve.js
@@ -0,0 +1,32 @@
+const steve = {
+  head: {
+    normal: require('@/emotes/steve/steve_head_normal.png'),
+  },
+  eyebrow: {
+    broom: require('@/emotes/steve/steve_eyebrow_broom.png'),
+    cap: require('@/emotes/steve/steve_eyebrow_cap.png'),
+    pringles: require('@/emotes/steve/steve_eyebrow_pringles.png'),
+  },
+  eyes: {
+    angry: require('@/emotes/steve/steve_eyes_angry.png'),
+    closed: require('@/emotes/steve/steve_eyes_closed.png'),
+    female: require('@/emotes/steve/steve_eyes_female.png'),
+    normal: require('@/emotes/steve/steve_eyes_normal.png'),
+    shocked: require('@/emotes/steve/steve_eyes_shocked.png'),
+    sleepy: require('@/emotes/steve/steve_eyes_sleepy.png'),
+  },
+  mouth: {
+    bigoh: require('@/emotes/steve/steve_mouth_bigoh.png'),
+    bigsmile: require('@/emotes/steve/steve_mouth_bigsmile.png'),
+    formal: require('@/emotes/steve/steve_mouth_formal.png'),
+    grin: require('@/emotes/steve/steve_mouth_grin.png'),
+    kissy: require('@/emotes/steve/steve_mouth_kissy.png'),
+    pout: require('@/emotes/steve/steve_mouth_pout.png'),
+    serious: require('@/emotes/steve/steve_mouth_serious.png'),
+    talk: require('@/emotes/steve/steve_mouth_talk.png'),
+    upset: require('@/emotes/steve/steve_mouth_upset.png'),
+    yell: require('@/emotes/steve/steve_mouth_yell.png'),
+  },
+}
+
+export default steve;
