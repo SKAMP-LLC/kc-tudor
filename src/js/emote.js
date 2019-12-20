808f574c9f5ebee0f6eeb71bd9b3843402e951c2
Devin Leonhart <devinleonhart@fastmail.com>
2019-12-19 16:44:20 -0800

    Remake emote generator using promises.

--- src/js/emote.js
+++ src/js/emote.js
@@ -15,36 +15,41 @@ export const assets = {
   steve: steve,
 };
 
-let mergedAssets = {
-  'sandy_normal': makeEmoji('sandy_normal', 'sandy', 'normal', 'normal', 'normal', 'smile'),
-  'sandy_excited': makeEmoji('sandy_excited', 'sandy', 'perked', 'normal', 'glee', 'smileopen'),
-  'sandy_humble': makeEmoji('sandy_awkward', 'sandy', 'droop', 'puzzled', 'lookaway', 'smile'),
-  'sandy_pout': makeEmoji('sandy_pout', 'sandy', 'droop', 'puzzled', 'lookaway', 'pout'),
-  'sandy_complain': makeEmoji('sandy_complain', 'sandy', 'puzzled', 'serious', 'lookaway', 'yell'),
-  'sandy_upset': makeEmoji('sandy_upset', 'sandy', 'normal', 'sad', 'shocked', 'yell'),
-  'sandy_rage': makeEmoji('sandy_rage', 'sandy', 'normal', 'angry', 'angry', 'yell'),
-  'sandy_cry': makeEmoji('sandy_cry', 'sandy', 'droop', 'sad', 'uu', 'pout'),
-  'steve_manager_exhausted': makeEmoji('steve_manager_exhausted', 'steve', 'normal', 'broom', 'sleepy', 'serious'),
-  'steve_manager_firm': makeEmoji('steve_manager_firm', 'steve', 'normal', 'broom', 'angry', 'formal'),
-  'steve_manager_inquisitive': makeEmoji('steve_manager_inquisitive', 'steve', 'normal', 'broom', 'normal', 'serious'),
-  'steve_manager_speaking': makeEmoji('steve_manager_speaking', 'steve', 'normal', 'broom', 'normal', 'talk'),
-};
-
 function makeEmoji(key, character, head, eyebrow, eyes, mouth) {
-  mergeImages([
-    assets[character].head[head],
-    assets[character].eyebrow[eyebrow],
-    assets[character].eyes[eyes],
-    assets[character].mouth[mouth],
-  ]).then((merged) => {
-    mergedAssets[key] = merged;
+  return new Promise((resolve, reject) => {
+    mergeImages([
+      assets[character].head[head],
+      assets[character].eyebrow[eyebrow],
+      assets[character].eyes[eyes],
+      assets[character].mouth[mouth],
+    ]).then((b64) => {
+      resolve({key: key, image: b64});
+    }, (err) => {
+      console.error('mergeImages failed.');
+      reject(err);
+    });
   });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  Object.keys(mergedAssets).forEach((emote) => {
-    document.querySelectorAll('.' + emote).forEach((elem) => {
-      elem.style.backgroundImage = "url(" + mergedAssets[emote] + ")";
+  Promise.all([
+    makeEmoji('sandy_normal', 'sandy', 'normal', 'normal', 'normal', 'smile'),
+    makeEmoji('sandy_excited', 'sandy', 'perked', 'normal', 'glee', 'smileopen'),
+    makeEmoji('sandy_awkward', 'sandy', 'droop', 'puzzled', 'lookaway', 'smile'),
+    makeEmoji('sandy_pout', 'sandy', 'droop', 'puzzled', 'lookaway', 'pout'),
+    makeEmoji('sandy_complain', 'sandy', 'puzzled', 'serious', 'lookaway', 'yell'),
+    makeEmoji('sandy_upset', 'sandy', 'normal', 'sad', 'shocked', 'yell'),
+    makeEmoji('sandy_rage', 'sandy', 'normal', 'angry', 'angry', 'yell'),
+    makeEmoji('sandy_cry', 'sandy', 'droop', 'sad', 'uu', 'pout'),
+    makeEmoji('steve_manager_exhausted', 'steve', 'normal', 'broom', 'sleepy', 'serious'),
+    makeEmoji('steve_manager_firm', 'steve', 'normal', 'broom', 'angry', 'formal'),
+    makeEmoji('steve_manager_inquisitive', 'steve', 'normal', 'broom', 'normal', 'serious'),
+    makeEmoji('steve_manager_speaking', 'steve', 'normal', 'broom', 'normal', 'talk'),
+  ]).then((emotes) => {
+    emotes.forEach((emote) => {
+      document.querySelectorAll('.' + emote['key']).forEach((elem) => {
+        elem.style.backgroundImage = "url(" + emote['image'] + ")";
+      });
     });
   });
 });
