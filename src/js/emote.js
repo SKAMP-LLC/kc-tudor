d379783d7049e04fd62b04ac2f8c2d1ea06504d9
Devin Leonhart <devinleonhart@fastmail.com>
2020-06-29 14:21:32 -0700

    Add optional name property.

--- src/js/emote.js
+++ src/js/emote.js
@@ -7,27 +7,32 @@ document.addEventListener("DOMContentLoaded", () => {
   let emotes = document.getElementsByTagName("emote");
 
   for(let emote of emotes) {
-    
     const attributes = emote.attributes;
-    const character = attributes.getNamedItem('character').value;
-    const dialog = emote.innerHTML;
-    const direction = attributes.getNamedItem('direction').value;
-    const emotion = attributes.getNamedItem('emotion').value;
+    const character = attributes.getNamedItem('character') ? attributes.getNamedItem('character').value : null;
+    const direction = attributes.getNamedItem('direction') ? attributes.getNamedItem('direction').value : null;
+    const emotion = attributes.getNamedItem('emotion') ? attributes.getNamedItem('emotion').value : null;
+    const name = attributes.getNamedItem('name') ? attributes.getNamedItem('name').value : null;
 
-    callAPI(character, emotion).then((data) => {
-      let view = {
-        character_name: character.charAt(0).toUpperCase() + character.toLowerCase().slice(1),
-        character_name_class: character.toLowerCase(),
-        character_dialog: dialog,
-        character_emotion: `${character.toLowerCase()}_${emotion}`,
-        objectURL: data,
-      };
-    
-      const emoteMarkup = Mustache.render(
-        direction === 'left' ? leftEmoticonTemplate : rightEmoticonTemplate, view, 'utf8'
-      );
-      
-      emote.outerHTML = emoteMarkup;
-    });
+    // Character, Direction and Emotion are required.
+    // Name is optional.
+    if(character && direction && emotion) {
+      const dialog = emote.innerHTML;
+
+      callAPI(character, emotion).then((data) => {
+        let view = {
+          character_name: name || character.charAt(0).toUpperCase() + character.toLowerCase().slice(1),
+          character_name_class: character.toLowerCase(),
+          character_dialog: dialog,
+          character_emotion: `${character.toLowerCase()}_${emotion}`,
+          objectURL: data,
+        };
+
+        const emoteMarkup = Mustache.render(
+          direction === 'left' ? leftEmoticonTemplate : rightEmoticonTemplate, view, 'utf8'
+        );
+
+        emote.outerHTML = emoteMarkup;
+      });
+    }
   }
 });
