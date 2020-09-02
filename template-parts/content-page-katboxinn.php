907630fbbb2755db53e83dbe45f516759d037ad8
Devin Leonhart <devinleonhart@fastmail.com>
2020-09-01 17:25:40 -0700

    Update Katbox Inn

--- template-parts/content-page-katboxinn.php
+++ template-parts/content-page-katboxinn.php
@@ -13,27 +13,37 @@
     <div class="box">
       <div class="inn_sign"></div>
 
+      <h2> &middot; My Life With Fel &middot; </h2>
+      <a href="https://www.mylifewithfel.com/index"><img src="https://kemono.cafe/wp-content/uploads/2020/09/mlwf-banner.jpg" alt="My Life With Fel"></a>
+      <a href="https://www.mylifewithfel.com/index">My Life With Fel</a>
+
       <h2> &middot; NotImportant &middot; </h2>
       <a href="https://replaycomic.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/06/replay-banner.png" alt="Replay Comic"></a>
       <a href="https://replaycomic.com/">Replay Comic</a>
 
       <h2> &middot; RazorFox &middot; </h2>
       <a href="https://www.draconiachronicles.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/07/dc-banner.jpg" alt="The Draconia Chronicles"></a>
-      <a href="https://www.draconiachronicles.com/">The Draconia Chronicles - https://www.draconiachronicles.com/</a>
+      <a href="https://www.draconiachronicles.com/">The Draconia Chronicles</a>
+
+      <h2> &middot; Sage &middot; </h2>
+      <a href="https://www.yoshsaga.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/09/yosh-banner.png" alt="Yosh! Saga"></a>
+      <a href="https://www.yoshsaga.com/">Yosh! Saga</a>
+      
+      <a href="https://www.artificialincident.com"><img src="https://kemono.cafe/wp-content/uploads/2020/09/ai-banner.png" alt="Artificial Incident"></a>
+      <a href="https://www.artificialincident.com">Artificial Incident</a>
+      
+      <h2> &middot; Skidd & Phuufy &middot; </h2>
+      <a href="https://uberquest.studiokhimera.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/09/uberquest-banner.png" alt="Uberquest"></a>
+      <a href="https://uberquest.studiokhimera.com/">Uber Quest</a>
+
+      <a href="https://mousechievous.studiokhimera.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/09/mousechievous-banner.png" alt="Mousechievous"></a>
+      <a href="https://mousechievous.studiokhimera.com/">Mousechievous</a>
 
       <h2> &middot; Nixie the Art Seal &middot; </h2>
       <a href="https://www.webtoons.com/en/challenge/debunkers/list?title_no=148475">Debunkers - https://www.webtoons.com/en/challenge/debunkers/list?title_no=148475</a>
 
-      <h2> &middot; Sage &middot; </h2>
-      <a href="https://www.yoshsaga.com/">Yosh! Saga - https://www.yoshsaga.com</a>
-      <a href="https://www.artificialincident.com/">Artificial Incident - https://www.artificialincident.com</a>
-
       <h2> &middot; Ambaaargh &middot; </h2>
       <a href="http://www.missmab.com/">Dan & Mab's Furry Adventures - http://www.missmab.com</a>
-
-      <h2> &middot; Skidd & Phuufy &middot; </h2>
-      <a href="https://uberquest.studiokhimera.com/">Uber Quest - https://uberquest.studiokhimera.com</a>
-      <a href="https://mousechievous.studiokhimera.com/">Mousechievous - https://mousechievous.studiokhimera.com</a>
 
       <h2> &middot; JonPonikv &middot; </h2>
       <a href="http://www.peterandcompany.com/">Peter & Company - http://www.peterandcompany.com</a>
@@ -56,6 +66,9 @@
 
       <h2> &middot; FOX.n.ROLL &middot; </h2>
       <a href="https://www.furaffinity.net/gallery/fox-pop/folder/688314/01-I-wont-go-in-Vietnam">Truck Off - https://www.furaffinity.net/gallery/fox-pop/folder/688314/01-I-wont-go-in-Vietnam</a>
+
+      <h2> &middot; Itsy Bitsy Adventures  &middot; </h2>
+      <a href="https://ringtailcafe.com/itsybitsy_webcomic/iba-ch1-00/">Itsy Bitsy Adventures - https://ringtailcafe.com/itsybitsy_webcomic/iba-ch1-00/</a>
     </div>
   </div>
 </article>
