ce5674e3a3dc32c11ae08a097c80c974853bba80
Devin Leonhart <devinleonhart@fastmail.com>
2021-02-03 11:29:18 -0800

    Update Bethellium Link

--- template-parts/content-carousel.php
+++ template-parts/content-carousel.php
@@ -61,7 +61,7 @@
     </div>
     <div class="comic_slot slot_12" data-title="Bethellium" data-rating="PG-13"
       data-description="Master alchemist Zoana is invited to the mystical city of Bethellium under false pretenses. Brought to save a life, why is her craft treated with fear and suspicion?">
-      <a href="http://dbcomics.darkblueworkshop.com/bethellium/"></a>
+      <a href="https://bethellium.darkbluecomics.com/"></a>
     </div>
     <div class="comic_slot slot_13" data-title="Knuckle Up" data-rating="Mature"
       data-description="After an incident involving an inexperienced space wizard, things got weird for space bounty hunters Hawk and Rushana. Now, in order to revert the 	&ldquo;curse&rdquo;, they must embark on a quest spanning the galaxy... Hang in there Hawke, you're not as hot as you think you look!">
@@ -121,7 +121,7 @@
     </div>
     <div class="comic_slot slot_12" data-title="Bethellium" data-rating="PG-13"
       data-description="Master alchemist Zoana is invited to the mystical city of Bethellium under false pretenses. Brought to save a life, why is her craft treated with fear and suspicion?">
-      <a href="http://dbcomics.darkblueworkshop.com/bethellium/"></a>
+      <a href="https://bethellium.darkbluecomics.com/"></a>
     </div>
     <div class="comic_slot slot_13" data-title="Knuckle Up" data-rating="Mature"
       data-description="After an incident involving an inexperienced space wizard, things got weird for space bounty hunters Hawk and Rushana. Now, in order to revert the 	&ldquo;curse&rdquo;, they must embark on a quest spanning the galaxy... Hang in there Hawke, you're not as hot as you think you look!">
@@ -181,7 +181,7 @@
     </div>
     <div class="comic_slot slot_12" data-title="Bethellium" data-rating="PG-13"
       data-description="Master alchemist Zoana is invited to the mystical city of Bethellium under false pretenses. Brought to save a life, why is her craft treated with fear and suspicion?">
-      <a href="http://dbcomics.darkblueworkshop.com/bethellium/"></a>
+      <a href="https://bethellium.darkbluecomics.com/"></a>
     </div>
     <div class="comic_slot slot_13" data-title="Knuckle Up" data-rating="Mature"
       data-description="After an incident involving an inexperienced space wizard, things got weird for space bounty hunters Hawk and Rushana. Now, in order to revert the 	&ldquo;curse&rdquo;, they must embark on a quest spanning the galaxy... Hang in there Hawke, you're not as hot as you think you look!">
--- template-parts/content-comic-list.php
+++ template-parts/content-comic-list.php
@@ -54,7 +54,7 @@
       <div class="thumbnail"></div>
       <span>Katbox Inn</span>
     </a>
-    <a href="http://dbcomics.darkblueworkshop.com/bethellium/" class="comic_list_item slot_12">
+    <a href="https://bethellium.darkbluecomics.com/" class="comic_list_item slot_12">
       <div class="thumbnail"></div>
       <span>Bethellium</span>
     </a>
