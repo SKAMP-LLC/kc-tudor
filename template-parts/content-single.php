cb722df910fcc14407afa76b9648158bde77bc2a
Devin Leonhart <devinleonhart@fastmail.com>
2021-02-16 17:51:16 -0800

    Remove CAN banner.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -33,8 +33,4 @@
 			}
 		?>
 	</div><!-- .entry-content -->
-  <hr />
-  <div class="bottom-content">
-    <script type="text/javascript" src="https://www.comicad.net/r/Tm4bD4vgca/"></script>
-  </div>
 </article><!-- #post-## -->
