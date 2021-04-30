724fe54b779dbf4fa5f7e480fdaa383648cc82e4
Devin Leonhart <devinleonhart@fastmail.com>
2021-04-30 16:16:22 -0700

    Update artist corner.

--- template-parts/content-page-artistcorner.php
+++ template-parts/content-page-artistcorner.php
@@ -22,6 +22,7 @@
             <li class="instagram"><a href="https://www.instagram.com/luxar.insta/"></a></li>
             <li class="twitter"><a href="https://piczel.tv/watch/luxarman"></a></li>
           </ul>
+          <p class="tagline">A delightful artist that focuses on feline cuties that provided the Kemono Cafe an exclusive interview.</p>
         </h2>
       </header>
       <div class="art-gallery">
@@ -34,8 +35,8 @@
         <a href="https://www.furaffinity.net/view/37866281/">
           <img src="https://kemono.cafe/wp-content/uploads/2021/04/lux_3.png">
         </a>
-        <a href="https://www.furaffinity.net/view/35063143/">
-          <img src="https://kemono.cafe/wp-content/uploads/2021/04/lux_4.png">
+        <a href="https://kemono.cafe/2021/03/22/the-luxar-interview/">
+          <img src="https://kemono.cafe/wp-content/uploads/2021/04/Luxar-Image-6.png">
         </a>
       </div>
     </div>
@@ -51,7 +52,9 @@
             <li class="deviant-art"><a href="https://www.deviantart.com/n8dogg5k"></a></li>
             <li class="twitter"><a href="https://n8dogg5k.blogspot.com/"></a></li>
           </ul>
+          <p class="tagline">A wonderfully talented digital artist that provided the Kemono Cafe with fan art at the beginning.</p>
         </h2>
+        <h3></h3>
       </header>
       <div class="art-gallery">
         <a href="https://www.deviantart.com/n8dogg5k/art/Bewb-Magnet-A-Mary-Gold-Tauros-Magnet-807243884">
@@ -63,8 +66,8 @@
         <a href="https://www.deviantart.com/n8dogg5k/art/Tail-Chaser-A-Bast-Katt-Sticker-807243115">
           <img src="https://kemono.cafe/wp-content/uploads/2021/04/n8_3.png">
         </a>
-        <a href="https://www.deviantart.com/n8dogg5k/art/Feeling-the-Crunch-759911372">
-          <img src="https://kemono.cafe/wp-content/uploads/2021/04/n8_4.png">
+        <a href="https://www.deviantart.com/kemonocafe/art/Milkshake-Surprise-by-N8Dogg5K-828944839">
+          <img src="https://kemono.cafe/wp-content/uploads/2021/04/ddpj5xj-64c45d5f-ccf6-44c1-bc8b-2741e43e445e.png">
         </a>
       </div>
     </div>
@@ -80,6 +83,7 @@
             <li class="fur-affinity"><a href="https://www.furaffinity.net/user/jeso/"></a></li>
             <li class="picarto"><a href="https://picarto.tv/jesonite"></a></li>
           </ul>
+          <p class="tagline">The legendary digital artist with superb mad skills in digital artwork that provided the Kemono Cafe with fan art.</p>
         </h2>
       </header>
       <div class="art-gallery">
@@ -92,8 +96,38 @@
         <a href="https://www.furaffinity.net/view/40098056/">
           <img src="https://kemono.cafe/wp-content/uploads/2021/04/jeso_3.jpg">
         </a>
-        <a href="https://www.furaffinity.net/view/38412970/">
-          <img src="https://kemono.cafe/wp-content/uploads/2021/04/jeso_4.jpg">
+        <a href="https://www.deviantart.com/kemonocafe/art/Kemono-Cafe-Splash-Page-by-Jesonite-846792665 ">
+          <img src="https://kemono.cafe/wp-content/uploads/2021/04/kemono_cafe_splash_page_by_jesonite_by_kemonocafe_de05peh-fullview.jpg">
+        </a>
+      </div>
+    </div>
+
+    <!-- ABlueDeer -->
+    <div class="box">
+      <header>
+        <img class="avatar" src="https://kemono.cafe/wp-content/uploads/2021/04/ABD-Avatar.png">
+        <h2>
+          ABlueDeer
+          <ul class="social">
+            <li class="twitter"><a href="https://twitter.com/abd_artist"></a></li>
+            <li class="fur-affinity"><a href="https://www.furaffinity.net/user/abluedeer"></a></li>
+            <li class="picarto"><a href="https://darkbluecomics.com/"></a></li>
+          </ul>
+          <p class="tagline">An awesome artist from Central American with stellar comics and awesome digital works that provided the Kemono Cafe with amazing link exchanges and an interview.</p>
+        </h2>
+      </header>
+      <div class="art-gallery">
+        <a href="https://www.deviantart.com/a-bluedeer/art/Patreon-Reward-Holly-Ice-Cream-852527782">
+          <img src="https://kemono.cafe/wp-content/uploads/2021/04/ABD-Image-1.jpg">
+        </a>
+        <a href="https://www.deviantart.com/a-bluedeer/art/Moonlace-Team-Summertime-760410187">
+          <img src="https://kemono.cafe/wp-content/uploads/2021/04/ABD-Image-2.jpg">
+        </a>
+        <a href="https://www.deviantart.com/a-bluedeer/art/Zoana-Ice-Cream-872043624">
+          <img src="https://kemono.cafe/wp-content/uploads/2021/04/ABD-Image-3.jpg">
+        </a>
+        <a href="https://kemono.cafe/2020/12/16/the-abd-interview/ ">
+          <img src="https://kemono.cafe/wp-content/uploads/2021/04/ABD-Image-4.jpg">
         </a>
       </div>
     </div>
