6db5e91c994fa9ec56c449dd4f6bc5958336788a
Devin Leonhart <devinleonhart@fastmail.com>
2021-08-01 18:45:59 -0700

    Trivial update to test pipeline.

--- template-parts/content-page-world.php
+++ template-parts/content-page-world.php
@@ -11,7 +11,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <div class="world-page">
     <header>
-      Fable
+      Map of Fable
     </header>
     <div class="map"></div>
     <div class="patrons emote-wrapper">
