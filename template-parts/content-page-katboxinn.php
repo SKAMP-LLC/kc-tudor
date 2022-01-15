de94ca6f84489c821a09d2a4865299cdf45d9e62
Devin Leonhart <devinleonhart@fastmail.com>
2022-01-15 12:53:08 -0800

    Remove all text from KB Inn.

--- template-parts/content-page-katboxinn.php
+++ template-parts/content-page-katboxinn.php
@@ -14,111 +14,35 @@
       <div class="inn_sign"></div>
 
       <section>
-
-        <h2> &middot; Nekonny &middot; </h2>
         <a href="https://cb.kemono.cafe/"><img src="https://kemono.cafe/wp-content/uploads/2021/12/cb.jpg" alt="Caribbean Blue"></a>
-        <a href="https://cb.kemono.cafe/">Caribbean Blue</a>
-
         <a href="https://pmp.kemono.cafe/"><img src="https://kemono.cafe/wp-content/uploads/2021/12/pmp.jpg" alt="Practice Makes Perfect"></a>
-        <a href="https://pmp.kemono.cafe/">Practice Makes Perfect</a>
-
-        <h2> &middot; Avencri &middot; </h2>
         <a href="https://tots.kemono.cafe/"><img src="https://kemono.cafe/wp-content/uploads/2021/12/tots.jpg" alt="Tina of the South"></a>
-        <a href="https://tots.kemono.cafe/">Tina of the South</a>
-
-        <h2> &middot; Leilani &middot; </h2>
         <a href="https://www.thedepthscomic.com/"><img src="https://kemono.cafe/wp-content/uploads/2021/04/The-Depths-Banner.png" alt="The Depths"></a>
-        <a href="https://www.thedepthscomic.com/">The Depths</a>
-
-        <h2> &middot; Roy Wolverine &middot; </h2>
         <a href="https://tapas.io/series/IOtters/info"><img src="https://kemono.cafe/wp-content/uploads/2021/04/Insignificant-Otters.jpg" alt="Insignificant Otters"></a>
-        <a href="https://tapas.io/series/IOtters/info">Insignificant Otters</a>
-
-        <h2> &middot; Bri Shepard &middot; </h2>
         <a href="http://evoncomics.com/"><img src="https://kemono.cafe/wp-content/uploads/2021/01/Evon-Banner.jpg" alt="Evon Comics"></a>
-        <a href="http://evoncomics.com/">Evon Comics</a>
-
-        <h2> &middot; Virmir &middot; </h2>
         <a href="https://crimsonflagcomic.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/12/cfbanner_300x200.png" alt="Crimson Flag"></a>
-        <a href="https://crimsonflagcomic.com/">Crimson Flag</a>
-
-        <h2> &middot; Kenno Arkkan &middot; </h2>
         <a href="https://www.mylifewithfel.com/index"><img src="https://kemono.cafe/wp-content/uploads/2020/09/mlwf-banner.jpg" alt="My Life With Fel"></a>
-        <a href="https://www.mylifewithfel.com/index">My Life With Fel</a>
-
-        <h2> &middot; NotImportant &middot; </h2>
         <a href="https://replaycomic.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/06/replay-banner.png" alt="Replay Comic"></a>
-        <a href="https://replaycomic.com/">Replay Comic</a>
-
-        <h2> &middot; Nixie the Art Seal &middot; </h2>
         <a href="http://kiwilemontea.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/09/debunkers-banner.png" alt="Debunkers"></a>
-        <a href="http://kiwilemontea.com/">Debunkers</a>
-
-        <h2> &middot; RazorFox &middot; </h2>
         <a href="https://www.draconiachronicles.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/07/dc-banner.jpg" alt="The Draconia Chronicles"></a>
-        <a href="https://www.draconiachronicles.com/">The Draconia Chronicles</a>
-
-        <h2> &middot; Jonathan & Whitney &middot; </h2>
         <a href="http://www.peterandcompany.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/11/peterandcompany-banner.jpg" alt="Peter & Company"></a>
-        <a href="http://www.peterandcompany.com/">Peter & Company</a>
-
         <a href="http://www.peterandwhitney.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/11/peterandwhitney-banner.jpg" alt="Peter & Whitney"></a>
-        <a href="http://www.peterandwhitney.com/">Peter & Whitney</a>
-
-        <h2> &middot; Sterling (aka SilverBlaze) &middot; </h2>
         <a href="https://ringtailcafe.com/itsybitsy_webcomic/iba-ch1-00/"><img src="https://kemono.cafe/wp-content/uploads/2020/11/iba-banner.png" alt="Itsy Bitsy Adventures"></a>
-        <a href="https://ringtailcafe.com/itsybitsy_webcomic/iba-ch1-00/">Itsy Bitsy Adventures</a>
       </section>
       <section>
-        <h2> &middot; Sonokido &middot; </h2>
         <a href="https://tapas.io/series/Ventura-City-Drifters/info"><img src="https://kemono.cafe/wp-content/uploads/2021/12/Ventura-City-Drifters.gif" alt="Ventura City Drifters"></a>
-        <a href="https://tapas.io/series/Ventura-City-Drifters/info">Ventura City Drifters</a>
-
-        <h2> &middot; Desert Fox &middot; </h2>
         <a href="https://desertfoxcomics.net/"><img src="https://kemono.cafe/wp-content/uploads/2021/12/desert-fox.jpg" alt="Desert Fox"></a>
-        <a href="https://desertfoxcomics.net/">Desert Fox</a>
-
-        <h2> &middot; Alexandria Bowersox &middot; </h2>
         <a href="https://huxcyn.com/"><img src="https://kemono.cafe/wp-content/uploads/2021/08/hux_banner.gif" alt="The Chronicles of Huxcyn"></a>
-        <a href="https://huxcyn.com/">The Chronicles of Huxcyn</a>
-
-        <h2> &middot; Scotty &middot; </h2>
         <a href="http://www.rayfoxthecomic.com/"><img src="https://kemono.cafe/wp-content/uploads/2021/04/Ray-Fox-Comic-Banner.jpg" alt="Ray Fox the Webcomic"></a>
-        <a href="http://www.rayfoxthecomic.com/">Ray Fox the Webcomic</a>
-
-        <h2> &middot; Jan &middot; </h2>
         <a href="https://www.tigerknight.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/11/Jans-Comics-Banner.png" alt="Jans Comics"></a>
-        <a href="https://www.tigerknight.com/">Jan's Comics</a>
-
-        <h2> &middot; Chu &middot; </h2>
         <a href="https://www.sdamned.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/09/slightlydamned-banner.png" alt="Slightly Damned"></a>
-        <a href="https://www.sdamned.com/">Slightly Damned</a>
-
-        <h2> &middot; Sage &middot; </h2>
         <a href="https://www.yoshsaga.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/09/yosh-banner.png" alt="Yosh! Saga"></a>
-        <a href="https://www.yoshsaga.com/">Yosh! Saga</a>
-
         <a href="https://www.artificialincident.com"><img src="https://kemono.cafe/wp-content/uploads/2020/09/ai-banner.png" alt="Artificial Incident"></a>
-        <a href="https://www.artificialincident.com">Artificial Incident</a>
-
-        <h2> &middot; Skidd & Phuufy &middot; </h2>
         <a href="https://uberquest.studiokhimera.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/09/uberquest-banner.png" alt="Uberquest"></a>
-        <a href="https://uberquest.studiokhimera.com/">Uber Quest</a>
-
         <a href="https://mousechievous.studiokhimera.com/"><img src="https://kemono.cafe/wp-content/uploads/2020/09/mousechievous-banner.png" alt="Mousechievous"></a>
-        <a href="https://mousechievous.studiokhimera.com/">Mousechievous</a>
-
-        <h2> &middot; FOX-POP &middot; </h2>
         <a href="https://www.furaffinity.net/gallery/fox-pop/folder/688314/01-I-wont-go-in-Vietnam"><img src="https://kemono.cafe/wp-content/uploads/2020/11/truckoff-banner.jpg" alt="Truck Off"></a>
-        <a href="https://www.furaffinity.net/gallery/fox-pop/folder/688314/01-I-wont-go-in-Vietnam">Truck Off</a>
-
-        <h2> &middot; Bone &middot; </h2>
         <a href="https://www.furaffinity.net/view/7966557"><img src="https://kemono.cafe/wp-content/uploads/2020/11/falsestart-banner.jpg" alt="False Start"></a>
-        <a href="https://www.furaffinity.net/view/7966557">False Start</a>
-
-        <h2> &middot; Wukrii &middot; </h2>
         <a href="https://www.wukrii.com/"><img src="https://kemono.cafe/wp-content/uploads/2021/01/Wukrii-Banner.png" alt="Wukrii"></a>
-        <a href="https://www.wukrii.com/">Wukrii</a>
       </section>
     </div>
   </div>
